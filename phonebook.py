e48fb36fb57052179de01b3dd3f39b0dc6be8257
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-11-14 15:32:14 -0500

    Adds phonebook app

---
+++ phonebook.py
@@ -0,0 +1,95 @@
+# DICT = {
+#         # 'Karen' : '7068598025',
+#         # 'Joe' : '7098527065'
+# }
+
+# Must have a menu
+
+
+# What do you want to do (1-5)?
+def print_main_menu():
+    print("""Electronic Phone Book
+=====================
+1. Look up an entry
+2. Set an entry
+3. Delete an entry
+4. List all entries
+5. Quit
+        """)
+    user_selection = int(input("What do you want to do (1-5)? "))
+    return user_selection
+# 1. Ask them for a person's name, then looks up persons phone number and prints it to the screen
+def lookup_entry(dictionary):
+    name = input("Please enter a name. ")
+    print()
+    print(dictionary[name])
+    print()
+# print(lookup_entry(DICT))
+
+# 2. Prompt them for a name and person's phone number
+def set_entry(dictionary):
+    print()
+    name = input("Please enter a name. ")
+    number = input("Please enter their phone number. ")
+    print()
+    dictionary[name] = number
+    return dictionary
+# set_entry(DICT)
+# print(DICT)
+# 3. Prompt them for a name and delete entire entry
+def delete_entry(dictionary):
+    name = input("Please enter a name. ")
+    print()
+    print(f"{name} has been deleted.")
+    print()
+    del dictionary[name]
+    return dictionary
+
+# delete_entry(DICT)
+# print(DICT)
+# 4. Go through all entries and print each to the terminal
+# realized I want something to print if dictionary is empty
+def print_dictionary(dictionary):
+    if len(dictionary) == 0:
+        print()
+        print("There is nothing in your phonebook. ")
+        print()
+    else:
+        print()
+        for key, value in dictionary.items():
+            print(f"{key} : {value}")
+        print()
+# print_dictionary(DICT)
+# 5. Quit the program
+def quit_program():
+    return False
+
+def main():
+    dictionary = {}
+    is_running = True
+    while is_running:
+        try:
+            user_selection = print_main_menu()
+            if user_selection == 1:
+                lookup_entry(dictionary)
+            elif user_selection == 2:
+                dictionary = set_entry(dictionary)
+            elif user_selection == 3:
+                dictionary = delete_entry(dictionary)
+            elif user_selection == 4:
+                print_dictionary(dictionary)
+            elif user_selection == 5:
+                is_running = quit_program()
+                print()
+                print("Thank you for using phonebook app. Please rate us on the app store. ")
+                print()
+            else:
+                print()
+                print("Please enter a number between 1 and 5.")
+                print()
+        except ValueError:
+            print()
+            print("Please enter a number between 1 and 5. ")
+            print()
+
+main()
