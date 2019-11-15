96888ead7ec704623978f29098c9962df7264149
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-11-15 10:28:28 -0500

    Updated to include all scenarios and tests.

--- friyay_exercise1.py
+++ friyay_exercise1.py
@@ -11,25 +11,29 @@ def check_arrays(array1, array2):
     dictionary2 = {}
     for number in array1:
         dictionary[number] = array1.count(number)
-    # print(dictionary)
+    print(dictionary)
     for number in array2:
         dictionary2[number] = array2.count(number)
-    # print(dictionary2)
+    print(dictionary2)
     if len(dictionary) != len(dictionary2):
         return False
-    try:
-        for key in dictionary.keys():
-            if dictionary[key] != dictionary2[key]:
-                return False
-        return True
-    except KeyError:
-        return False
+    # try:
+    for key in dictionary.keys():
+        if dictionary[key] != dictionary2[key]:
+            return False
+    return True
+    # except KeyError:
+    #     return False
 
 # print(check_arrays([1,2,3,4], [1,2,3,4, 5]))
-print(check_arrays([1,2,3,4], [1,2,3,4]))
-print(check_arrays([1,2,3,4], [1,4,5,6]))
-print(check_arrays([1,2,3,4],[1,4,4,2]))
-print(check_arrays([1,2,3,4],[1,4,3,2]))
-print(check_arrays([1,2,3,4,5],[1,2,3,4]))
+print(check_arrays([1,2,3,5], [1,2,3,4]))
+print(check_arrays([1,2,3,4], [1,2,3,4])) # True
+print(check_arrays([1,2,3,4], [1,4,5,6])) # False
+print(check_arrays([1,2,3,4],[1,4,4,2])) # False
+print(check_arrays([1,2,3,4],[1,4,3,2])) # True
+print(check_arrays([1,2,3,4,5],[1,2,3,4])) # False
 # You count the number of each number
 # Then compare the len of the dictionaries to make sure they have the same number of numbers
+# find the count of each number in an array
+# find the count of each number in array2
+# 
